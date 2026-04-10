1bb099477fc92e06f9775b8f03e95a5a6c17a510
Kaua Felix <kfelixcosta11@gmail.com>
2026-04-10 16:35:52 -0300

    feat: adiciona service de funcionario que vai ser implementada

---
+++ src/main/java/com/weg/centroweg/cardinalidade/app/service/FuncionarioService.java
@@ -0,0 +1,9 @@
+package com.weg.centroweg.cardinalidade.app.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class FuncionarioService {
+}
