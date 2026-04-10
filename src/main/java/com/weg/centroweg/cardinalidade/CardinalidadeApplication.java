8e61505877cfec31ab7d789cde0d8e603a0b0c69
Kaua Felix <kfelixcosta11@gmail.com>
2026-04-10 16:35:59 -0300

    main

---
+++ src/main/java/com/weg/centroweg/cardinalidade/CardinalidadeApplication.java
@@ -0,0 +1,13 @@
+package com.weg.centroweg.cardinalidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardinalidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardinalidadeApplication.class, args);
+	}
+
+}
