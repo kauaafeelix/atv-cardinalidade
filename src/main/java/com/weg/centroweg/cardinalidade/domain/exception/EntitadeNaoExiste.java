dc52895dc2543fbee8a8cbadef497222d6e4bd09
Kaua Felix <kfelixcosta11@gmail.com>
2026-04-10 20:34:04 -0300

    feat: adiciona exception customizada para tratamento de erros

---
+++ src/main/java/com/weg/centroweg/cardinalidade/domain/exception/EntitadeNaoExiste.java
@@ -0,0 +1,7 @@
+package com.weg.centroweg.cardinalidade.domain.exception;
+
+public class EntitadeNaoExiste extends RuntimeException {
+    public EntitadeNaoExiste(String message) {
+        super(message);
+    }
+}
